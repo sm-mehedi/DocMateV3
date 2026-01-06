fecbf02399469c37d906f2e835cbad67ae36179e
S. M MEHEDI HASAN <121720300+sm-mehedi@users.noreply.github.com>
2026-01-06 09:26:02 +0600

    Mehedi branch (#2)
    * DocMate database creation
    
    * Database Update: User table
    
    * patients table is created
    
    ---------
    
    Co-authored-by: sm-mehedi <hasanmehedi63395@gmail,com>

--- database/docmate.sql
+++ database/docmate.sql
@@ -1,2 +1,20 @@
 CREATE DATABASE docmate;
 USE docmate;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin','doctor','patient') NOT NULL
+);
+CREATE TABLE patients (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20),
+    address VARCHAR(255),
+    health_issues VARCHAR(255),
+    emergency VARCHAR(20),
+    nid VARCHAR(20),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);

1d232bf416ed5bc827ea99d422af28d42a82b0bb
sm-mehedi <hasanmehedi63395@gmail,com>
2026-01-06 09:16:32 +0600

    Database Update: User table

--- database/docmate.sql
+++ database/docmate.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE docmate;
 USE docmate;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin','doctor','patient') NOT NULL
+);

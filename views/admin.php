4073b037292e2f651ce405b90df43d903458e1c5
S. M MEHEDI HASAN <121720300+sm-mehedi@users.noreply.github.com>
2026-01-16 20:54:53 +0600

    Anika branch (#12)
    * Public folder+mvc was created + medicine data.json
    
    * Query updated: Added users through query
    
    * created admin css and view file for admin [dashboard]
    
    * Added condition only admin role can access this page
    
    ---------
    
    Co-authored-by: Taha-chowdhury <zamal19642@gmail.com>

--- views/admin.php
+++ views/admin.php
@@ -1,3 +1,9 @@
 <?php
 session_start();
 require "../app/config/database.php";
+
+if(!isset($_SESSION['user_id']) || ($_SESSION['role'] ?? '') !== 'admin'){
+    header("Location: ../public/login.php");
+    exit;
+}
+require "../app/config/database.php";

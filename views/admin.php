c488facc024fd7c761095d97532377fa4f958235
Taha-chowdhury <zamal19642@gmail.com>
2026-01-16 20:52:21 +0600

    Added condition only admin role can access this page

--- views/admin.php
+++ views/admin.php
@@ -1,3 +1,8 @@
 <?php
 session_start();
 require "../app/config/database.php";
+
+if(!isset($_SESSION['user_id']) || ($_SESSION['role'] ?? '') !== 'admin'){
+    header("Location: ../public/login.php");
+    exit;
+}

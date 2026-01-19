4c2cabf62621e456600b05fa7580546d0bb5e39b
sm-mehedi <hasanmehedi63395@gmail,com>
2026-01-20 00:48:43 +0600

    Add hasBooking method to check if patient has an active booking with a doctor

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -5,3 +5,10 @@ class Booking {
     public function __construct($conn){
         $this->conn = $conn;
     }
+     public function hasBooking($doctor_id, $patient_id){
+        $q = $this->conn->prepare(
+            "SELECT * FROM bookings WHERE doctor_id=? AND patient_id=? AND status='booked'"
+        );
+        $q->execute([$doctor_id, $patient_id]);
+        return $q->fetch(PDO::FETCH_ASSOC);
+    }
